0022f77db3b144cbf149f098e0ee9e36cdeade11
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2022-07-21 13:54:15 +0700

    Create isAnagram.cpp

---
+++ isAnagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        int a[26] = {0};
+        
+        for(int i = 0; i < s.size(); i++){
+            a[int(s[i] - 'a')]++;
+        }
+        
+        for(int i = 0; i < t.size(); i++){
+            a[int(t[i] - 'a')]--;
+        }
+        
+        for(int i = 0; i < 26; i++)
+            if(a[i] != 0 ) return false;
+        return true;
+    }
+};

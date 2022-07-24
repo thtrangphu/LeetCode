3d0090c9bd3278130a00c322a186d7f6ca3fd0ef
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2022-07-24 12:19:57 +0700

    Update 12checkBalanced.cpp

--- 12checkBalanced.cpp
+++ 12checkBalanced.cpp
@@ -9,7 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
+Cách khác 
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (!root)
+            return 0;
+        return 1 + max(height(root->left), height(root->right));
+    }
 
+    bool isBalanced(TreeNode* root) {
+        if (!root) return 1;
+        int l = height(root->left);
+        int r = height(root->right);
+        // Với mỗi node của cây phải kiểm tra nó cân bằng k 
+        if(isBalanced(root->left) && isBalanced(root->right) && (abs(l - r) <= 1))
+           return true;
+        return false;     
+    }
+};
+
+*/
 class Solution {
 public:
     // Kiểm tra chiều cao của mỗi nút xem có cân bằng hay k 

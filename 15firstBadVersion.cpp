d5d806776bb2676ce0841810fe5ea64c207e0571
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2022-07-30 00:28:22 +0700

    Create 15firstBadVersion.cpp

---
+++ 15firstBadVersion.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l = 1, h = n, mid;
+        while (l < h) {
+            mid = l + (h - l) / 2;
+            if (isBadVersion(mid)) h = mid;
+            else l = mid+1;
+        }
+        return l;
+    }
+};

ed0be61550659006ad0568afd0c6db15b3298b6c
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2022-07-30 00:19:01 +0700

    Update 17climbStairs.cpp

--- 17climbStairs.cpp
+++ 17climbStairs.cpp
@@ -5,7 +5,7 @@ public:
         a[1] = 1;
         a[2] = 2;
         
-        for(int i = 3; i < n; i++){
+        for(int i = 3; i <= n; i++){
             a[i] = a[i - 1] + a[i - 2]; 
         }
         

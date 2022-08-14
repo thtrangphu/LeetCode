588d622009e5ac239e0cb0c4f42c13c54c630127
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2022-08-14 16:53:25 +0700

    Update 37missingNumber.cpp

--- 37missingNumber.cpp
+++ 37missingNumber.cpp
@@ -13,3 +13,43 @@ public:
         return sum;
     }
 };
+/*
+int missingNumber(vector<int>& nums) {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    if(n==0) return 0;        
+    int l=0, r=n;
+    while(l<=r){
+        int m =(l+r)/2;
+        if(m<n && nums[m]==m){
+            l=m+1;
+        }
+        else if(m<n){
+            r=m-1;
+        }else{
+            return n;
+        }
+    }
+    return l;     
+}
+
+int missingNumber(vector<int>& nums) {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    if(n==0) return 0;        
+    int l=0, r=n;
+    while(l<=r){
+        int m =(l+r)/2;
+        if(m<n && nums[m]==m){
+            l=m+1;
+        }
+        else if(m<n){
+            r=m-1;
+        }else{
+            return n;
+        }
+    }
+    return l;     
+}
+
+*/

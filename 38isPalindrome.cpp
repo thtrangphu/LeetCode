6352e3bce20da34ae476ded71a17bcd75973f327
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2022-08-15 00:28:19 +0700

    Update 38isPalindrome.cpp

--- 38isPalindrome.cpp
+++ 38isPalindrome.cpp
@@ -31,3 +31,17 @@ public:
         return true;
     }
 };
+// class Solution {
+//     public:
+//     bool isPalindrome(int x) {
+//         if (x < 0)
+//             return false;
+//         int y = 0, z = x; 
+//         while (z) {
+//             y *= 10;
+//             y += z % 10;
+//             z /= 10;
+//         }
+//         return x == y;
+//     }
+// };
